f417ba4d9687962e76d8178bca6ce0fdb5f52c20
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2025-02-06 20:13:25 -0300

    Criação do PizzaController

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzariaAspNet.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
